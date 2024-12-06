4ac0e19eac613d3adf41fe9c8b182cad2524c60a
James (DPC) <69664712+JamesDPC@users.noreply.github.com>
2024-12-06 17:02:22 +1100

    Add global search form for TemplateGlobalProvider

---
+++ src/Models/Configuration.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace NSWDPC\Waratah\Typesense\Models;
+
+use NSWDPC\Typesense\CMS\Models\TypesenseSearchPage;
+use NSWDPC\Typesense\CMS\Controllers\TypesenseSearchPageController;
+use NSWDPC\Search\Forms\Forms\SearchForm;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\View\TemplateGlobalProvider;
+
+/**
+ * Provides a hero search element conforming to the NSW Design System Hero Search Component
+ */
+class Configuration implements TemplateGlobalProvider {
+
+    use Configurable;
+
+    /**
+     * Provide a global search form
+     */
+    public static function get_global_search_form(): ?SearchForm {
+        $form = null;
+        if($searchPage = TypesenseSearchPage::get()->filter(['IsGlobalSearch' => 1])->first()) {
+            $controller = TypesenseSearchPageController::create($searchPage);
+            return SearchForm::create(
+                $controller,
+                'GlobalSearchForm'
+            )->setFormMethod('GET')
+            ->setFormAction($controller->Link())
+            ->disableSecurityToken();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Provide template global variables
+     */
+    public static function get_template_global_variables()
+    {
+        return [
+            'SearchForm' => 'get_global_search_form',// provided for BC
+            'GlobalSearchForm' => 'get_global_search_form'
+        ];
+    }
+
+
+}
